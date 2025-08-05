c6cb56de4d94c3f8af1a270efafbd026fd25f001
jinzoon <186304379+jinzoon@users.noreply.github.com>
2025-08-05 19:16:21 +0900

    Create initial grid
    I added a nested for loop inside the loop that creates the rows. This
    new loop creates the same amount of columns as rows. Both the columns
    and rows use flexbox so as to grow to fill the containers.

--- script.js
+++ script.js
@@ -7,8 +7,16 @@ drawGrid(numberOfSides,gridContainer);
 function drawGrid(numberOfSides, gridContainer) {
     for (let i = 0; i < numberOfSides; i++) {
         const newDiv = document.createElement("div");
-        
+        newDiv.style.flexGrow = "1";
+        newDiv.id = "Row_"+i;
+        newDiv.style.display = "flex";
         gridContainer.appendChild(newDiv);
+        for (let x = 0; x < numberOfSides; x++) {
+            const newSubDiv = document.createElement("div");
+            newSubDiv.style.flexGrow = "1";
+            newSubDiv.id = i+","+x;
+            newDiv.appendChild(newSubDiv);
+        }
         
     }
 }
